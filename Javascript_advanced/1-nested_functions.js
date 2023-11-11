411f6aa30a87090285bfb90f35be492d644afaf9
carlalap <carlos3_alarcon@hotmail.com>
2023-11-11 12:36:47 -0600

    task 1. Closure Scope Chain added

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,30 @@
+/* Task 1. Closure Scope Chain  
+@description: Running the script should display
+three popups one by one with the text:
+ Welcome, Welcome Holberton, and Welcome Holberton!*/
+
+/* Global Variable */
+
+let globalVariable = "Welcome";
+
+/* outer function */
+function outer () {
+    alert(globalVariable);
+
+    let course = "Holberton"
+
+    /* inner function */
+    function inner () {
+        alert(globalVariable + ' ' + course);
+
+        let exclamation = "!";
+
+        /* function that show an alert with three concatenate valiables */
+        function inception() {
+            alert(globalVariable + ' ' + course + exclamation);
+        }
+        inception();
+    }
+    inner();
+}
+outer();
